8574e90cadfa8229e21a7397b6f11555b96f5dad
gangjian <gangjian.mail@qq.com>
2015-08-12 14:48:38 +0800

    2015/08/12 8月11日,井泉服务区现场, 锅炉水温, 水表, 测试代码.

--- ConnectTest/ConnectTest/Program.cs
+++ ConnectTest/ConnectTest/Program.cs
@@ -16,7 +16,10 @@ namespace ConnectTest
 			Console.WriteLine("Hello World!");
 			try
 			{
-				Connect("192.168.0.7", 26);
+                // 水表
+				Connect("10.62.1.206", 26);
+                // 锅炉温度
+                // Connect("10.62.1.204", 26);
 			}
 			catch (Exception ex)
 			{
@@ -36,8 +39,21 @@ namespace ConnectTest
 			Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 			socket.Connect(ipe);
 
-			byte[] sendBytes = { 0x10, 0x07, 0x00, 0x00, 0x00, 0x01, 0x0a };
-			for (int i = 0; i < 3; i++)
+//			byte[] sendBytes = { 0x10, 0x07, 0x00, 0x00, 0x00, 0x01, 0x0a };
+            // 水表
+            byte[] tmpBytes = { 0x08, 0x03, 0x02, 0x02, 0x00, 0x02 };
+            // 锅炉温度
+            //byte[] tmpBytes = { 0x08, 0x03, 0x00, 0x01, 0x00, 0x01 };
+
+            UInt16 crc16 = CRC16(tmpBytes, 6);
+            byte crcLowByte = (byte)(crc16 & 0x00FF);
+            byte crcHighByte = (byte)((crc16 & 0xFF00) >> 8);
+            // 水表
+            byte[] sendBytes = { 0x08, 0x03, 0x02, 0x02, 0x00, 0x02, crcLowByte, crcHighByte };
+            // 锅炉水温
+            //byte[] sendBytes = { 0x08, 0x03, 0x00, 0x01, 0x00, 0x01, crcLowByte, crcHighByte };
+
+			for (int i = 0; i < 1; i++)
 			{
 				socket.Send(sendBytes);
 
@@ -51,6 +67,29 @@ namespace ConnectTest
 				System.Threading.Thread.Sleep(800);
 			}
 		}
+
+        public static UInt16 CRC16(Byte[] dat, int count)
+        {
+            uint crc = 0xFFFF;
+            int i, j;
+            for (j = 0; j < count; j++)
+            {
+                crc = crc ^ dat[j];
+                for (i = 0; i < 8; i++)
+                {
+                    if ((crc & 0x0001) > 0)
+                    {
+                        crc = crc >> 1;
+                        crc = crc ^ 0xa001;
+                    }
+                    else
+                    {
+                        crc = crc >> 1;
+                    }
+                }
+            }
+            return (UInt16)(crc);
+        }
 
 	}
 }
