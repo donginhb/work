ab191582c30a19bf32a64e608f9773b9e621c91e
gangjian <gangjian.mail@qq.com>
2015-09-23 13:44:51 +0800

    Ver 20150923

--- ServiceAreaClient/ServiceAreaClientGUI/Form1.Designer.cs
+++ ServiceAreaClient/ServiceAreaClientGUI/Form1.Designer.cs
@@ -494,7 +494,7 @@
 			this.MaximizeBox = false;
 			this.Name = "Form1";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "ServiceAreaClient_150914";
+			this.Text = "ServiceAreaClient_150923";
 			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
 			this.Load += new System.EventHandler(this.Form1_Load);
 			this.groupBox1.ResumeLayout(false);
