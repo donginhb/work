0db9a7102c25b570173eebc9cea61346589fcfe1
gangjian <gangjian.mail@qq.com>
2016-08-28 21:58:33 +0800

    改主窗体标题: ServiceAreaClient_20160828

--- ServiceAreaClient/ServiceAreaClientGUI/Form1.Designer.cs
+++ ServiceAreaClient/ServiceAreaClientGUI/Form1.Designer.cs
@@ -494,7 +494,7 @@
 			this.MaximizeBox = false;
 			this.Name = "Form1";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "ServiceAreaClient_20160827";
+			this.Text = "ServiceAreaClient_20160828";
 			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
 			this.Load += new System.EventHandler(this.Form1_Load);
 			this.groupBox1.ResumeLayout(false);
